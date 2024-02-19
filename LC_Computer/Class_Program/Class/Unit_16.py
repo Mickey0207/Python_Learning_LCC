a1dfac99714fca9dd1b8eb67703013f8db948cc7
Mickey Lin <notelin0207@gmail.com>
2024-02-20 02:13:42 +0800

    Unit_16

---
+++ LC_Computer/Class_Program/Class/Unit_16.py
@@ -0,0 +1,27 @@
+﻿import requests
+import pandas as pd
+
+def get_web():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (<system-information>) <platform> (<platform-details>) <extensions>'
+        }
+    url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
+
+    dfs = pd.read_html(url)
+    
+    df = dfs[0]
+    df.columns = df.columns.get_level_values(1)
+    
+    df1 = df.iloc[:,0:5]
+    df1.columns = ['幣別','現金匯率_本行買入','現金匯率_本行賣出','即期匯率_本行買入','即期匯率_本行賣出']    
+    df1.to_csv('C:/Users/notel/OneDrive/IE/GitHub/Python_Learning_LCC/LC_Computer/Class_Program/Class/Other File/currency.csv')
+
+    print(df1)
+
+def date_practise():
+    df = pd.read_csv('C:/Users/notel/OneDrive/IE/GitHub/Python_Learning_LCC/LC_Computer/Class_Program/Homework/0210.csv')
+    df.index = pd.to_datetime(df.index, format = '%Y%m%d')
+
+    
+    
+date_practise()
---
+++ LC_Computer/Class_Program/Homework/currency_history.py
@@ -0,0 +1,38 @@
+﻿import requests
+import pandas as pd
+import plotly.graph_objects as g
+
+def get_web():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (<system-information>) <platform> (<platform-details>) <extensions>'
+        }
+    url = 'https://rate.bot.com.tw/xrt/quote/ltm/USD'
+
+    dfs = pd.read_html(url)
+    
+    df = dfs[0]
+    df.columns = df.columns.get_level_values(1)
+    df1 = df.iloc[0:,lambda df: [0,4,5]]
+    df1.columns = ['日期','即期匯率_本行買入','即期匯率_本行賣出']
+    dfr = df1.iloc[::-1]    #座標反轉
+    dfr.to_csv('C:/Users/notel/OneDrive/IE/GitHub/Python_Learning_LCC/LC_Computer/Class_Program/Homework/0210.csv', encoding = 'utf-8-sig')
+
+    result = g.Figure()
+    result.add_trace(
+        g.Scatter(
+            x = dfr['日期'],
+            y = dfr['即期匯率_本行買入'],
+            name = '即期匯率_本行買入',
+        )
+    )
+    result.add_trace(
+        g.Scatter(
+            x = dfr['日期'],
+            y = dfr['即期匯率_本行賣出'],
+            name = '即期匯率_本行賣出',
+        )
+    )
+    result.show()
+ 
+    
+get_web()
